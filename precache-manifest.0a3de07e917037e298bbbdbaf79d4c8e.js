4945c67f21bb1f2073f8a78e4b3414084961b175
Dozornii <dozornii@gmail.COM>
2020-12-04 20:47:31 +0200

    Updates

---
+++ precache-manifest.0a3de07e917037e298bbbdbaf79d4c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ea7215df9b942c18af8aab375c13a4",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1f26e30b0f7c5a6899c9",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "06ffc64ce5ba6e834c34",
+    "url": "/contactmanager/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1f26e30b0f7c5a6899c9",
+    "url": "/contactmanager/static/js/2.11a223bf.chunk.js"
+  },
+  {
+    "revision": "06ffc64ce5ba6e834c34",
+    "url": "/contactmanager/static/js/main.f0f702dc.chunk.js"
+  },
+  {
+    "revision": "ee6f160243834146c013",
+    "url": "/contactmanager/static/js/runtime~main.edb04555.js"
+  }
+]);
